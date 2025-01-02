8ab4931fbed8f0495336cdb616b7bbc28bfb1908
javedalihosamani <javed.hosamani007@gmail.com>
2025-01-02 18:05:44 +0530

    Kannada Translate

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,77 +4,77 @@ const Home = () => {
         <div className="container my-5">
             <div className="row my-5">
                 <div className="col-12 text-center">
-                    <h1 className="display-1">Sufism</h1>
-                    <p>This article is about the body of mystical practice. For other uses, see Sufism (disambiguation). "Sufi" redirects here. For sanctification in Islam, see Tazkiyah.</p>
+                    <h1 className="display-1">ಸೂಫಿಸಂ</h1>
+                    <p>ಈ ಲೇಖನವು ಅತೀಂದ್ರಿಯ ಅಭ್ಯಾಸದ ದೇಹದ ಬಗ್ಗೆ. ಇತರ ಬಳಕೆಗಳಿಗಾಗಿ, ಸೂಫಿಸಂ (ದ್ವಂದ್ವ ನಿವಾರಣೆ) ನೋಡಿ. "ಸೂಫಿ" ಇಲ್ಲಿಗೆ ಮರುನಿರ್ದೇಶಿಸುತ್ತದೆ. ಇಸ್ಲಾಂನಲ್ಲಿ ಪವಿತ್ರೀಕರಣಕ್ಕಾಗಿ, ತಜ್ಕಿಯಾವನ್ನು ನೋಡಿ.</p>
                 </div>
             </div>
             <div className="row">
                 <div className="col-12">
                     <div className="lead">
-                        <p>Sufism (Arabic: الصوفية, romanized: al-Ṣūfiyya or Arabic: التصوف, romanized: al-Taṣawwuf) is a mystic body of religious practice found within Islam which is characterized by a focus on Islamic purification, spirituality, ritualism, and asceticism.</p>
+                        <p>ಸೂಫಿಸಂ (ಅರೇಬಿಕ್: الصوفية, ರೋಮನೈಸ್ಡ್: ಅಲ್-ಷೂಫಿಯಾ ಅಥವಾ ಅರೇಬಿಕ್: التصوف, ರೋಮನೈಸ್ಡ್: ಅಲ್-ತಸವ್ವುಫ್) ಇಸ್ಲಾಮಿನಲ್ಲಿ ಕಂಡುಬರುವ ಧಾರ್ಮಿಕ ಆಚರಣೆಯ ಒಂದು ಅತೀಂದ್ರಿಯ ದೇಹವಾಗಿದ್ದು, ಇದು ಇಸ್ಲಾಮಿಕ್ ಶುದ್ಧೀಕರಣ, ಆಧ್ಯಾತ್ಮಿಕತೆ, ಧಾರ್ಮಿಕತೆ ಮತ್ತು ತಪಸ್ವಿಗಳ ಮೇಲೆ ಕೇಂದ್ರೀಕೃತವಾಗಿದೆ.</p>
                         <p>
-                        Practitioners of Sufism are referred to as "Sufis" (from صُوفِيّ, ṣūfīy), and historically typically belonged to "orders" known as tariqa (pl. ṭuruq) — congregations formed around a grand wali (saint) who would be the last in a chain of successive teachers linking back to Muhammad, with the goal of undergoing tazkiya (self purification) and the hope of reaching the spiritual station of ihsan. The ultimate aim of Sufis is to seek the pleasure of God by endeavoring to return to their original state of purity and natural disposition, known as fitra.
+                        ಸೂಫಿಸಂನ ಅಭ್ಯಾಸಿಗಳನ್ನು "ಸೂಫಿಗಳು" (صُوفِيّ, ṣūfīy ನಿಂದ) ಎಂದು ಉಲ್ಲೇಖಿಸಲಾಗುತ್ತದೆ ಮತ್ತು ಐತಿಹಾಸಿಕವಾಗಿ ವಿಶಿಷ್ಟವಾಗಿ ತಾರಿಕಾ (pl. ṭuruq) ಎಂದು ಕರೆಯಲ್ಪಡುವ "ಆರ್ಡರ್‌ಗಳಿಗೆ" ಸೇರಿದವರು - ಒಬ್ಬ ಮಹಾನ್ ವಾಲಿ (ಸಂತ) ಸುತ್ತಲೂ ಸಭೆಗಳು ರಚಿಸಲ್ಪಟ್ಟವು. ಒಳಗೊಳ್ಳುವ ಗುರಿಯೊಂದಿಗೆ ಮುಹಮ್ಮದ್‌ಗೆ ಮತ್ತೆ ಸಂಪರ್ಕ ಕಲ್ಪಿಸುವ ಸತತ ಶಿಕ್ಷಕರ ಸರಪಳಿ ತಜ್ಕಿಯಾ (ಸ್ವಯಂ ಶುದ್ಧೀಕರಣ) ಮತ್ತು ಇಹ್ಸಾನ್ ಆಧ್ಯಾತ್ಮಿಕ ನಿಲ್ದಾಣವನ್ನು ತಲುಪುವ ಭರವಸೆ. ಫಿತ್ರಾ ಎಂದು ಕರೆಯಲ್ಪಡುವ ಅವರ ಮೂಲ ಶುದ್ಧತೆ ಮತ್ತು ನೈಸರ್ಗಿಕ ಸ್ವಭಾವಕ್ಕೆ ಮರಳಲು ಪ್ರಯತ್ನಿಸುವ ಮೂಲಕ ದೇವರ ಆನಂದವನ್ನು ಹುಡುಕುವುದು ಸೂಫಿಗಳ ಅಂತಿಮ ಗುರಿಯಾಗಿದೆ.
                         </p>
                         <p>
-                        Sufism emerged early on in Islamic history, partly as a reaction against the expansion of the early Umayyad Caliphate (661–750) and mainly under the tutelage of Hasan al-Basri. Although Sufis were opposed to dry legalism, they strictly observed Islamic law and belonged to various schools of Islamic jurisprudence and theology. Although the overwhelming majority of Sufis, both pre-modern and modern, remain adherents of Sunni Islam, certain strands of Sufi thought transferred over to the ambits of Shia Islam during the late medieval period. This particularly happened after the Safavid conversion of Iran under the concept of irfan. Important focuses of Sufi worship include dhikr, the practice of remembrance of God. Sufis also played an important role in spreading Islam through their missionary and educational activities.
+                        ಇಸ್ಲಾಮಿಕ್ ಇತಿಹಾಸದ ಆರಂಭದಲ್ಲಿ ಸೂಫಿಸಂ ಹೊರಹೊಮ್ಮಿತು, ಭಾಗಶಃ ಆರಂಭಿಕ ಉಮಯ್ಯದ್ ಕ್ಯಾಲಿಫೇಟ್ (661-750) ವಿಸ್ತರಣೆಯ ವಿರುದ್ಧ ಪ್ರತಿಕ್ರಿಯೆಯಾಗಿ ಮತ್ತು ಮುಖ್ಯವಾಗಿ ಹಸನ್ ಅಲ್-ಬಸ್ರಿ ಅವರ ಮಾರ್ಗದರ್ಶನದಲ್ಲಿ. ಸೂಫಿಗಳು ಒಣ ಕಾನೂನುಬದ್ಧತೆಯನ್ನು ವಿರೋಧಿಸುತ್ತಿದ್ದರೂ, ಅವರು ಇಸ್ಲಾಮಿಕ್ ಕಾನೂನನ್ನು ಕಟ್ಟುನಿಟ್ಟಾಗಿ ಪಾಲಿಸಿದರು ಮತ್ತು ಇಸ್ಲಾಮಿಕ್ ನ್ಯಾಯಶಾಸ್ತ್ರ ಮತ್ತು ದೇವತಾಶಾಸ್ತ್ರದ ವಿವಿಧ ಶಾಲೆಗಳಿಗೆ ಸೇರಿದವರು.ಬಹುಪಾಲು ಸೂಫಿಗಳು, ಆಧುನಿಕ-ಪೂರ್ವ ಮತ್ತು ಆಧುನಿಕ ಎರಡೂ, ಸುನ್ನಿ ಇಸ್ಲಾಂನ ಅನುಯಾಯಿಗಳಾಗಿ ಉಳಿದಿದ್ದರೂ, ಸೂಫಿ ಚಿಂತನೆಯ ಕೆಲವು ಎಳೆಗಳು ಮಧ್ಯಕಾಲೀನ ಅವಧಿಯ ಕೊನೆಯಲ್ಲಿ ಶಿಯಾ ಇಸ್ಲಾಂನ ಮಹತ್ವಾಕಾಂಕ್ಷೆಗಳಿಗೆ ವರ್ಗಾಯಿಸಲ್ಪಟ್ಟವು. ಇರ್ಫಾನ್ ಪರಿಕಲ್ಪನೆಯಡಿಯಲ್ಲಿ ಇರಾನ್‌ನ ಸಫಾವಿಡ್ ಪರಿವರ್ತನೆಯ ನಂತರ ಇದು ವಿಶೇಷವಾಗಿ ಸಂಭವಿಸಿತು.ಸೂಫಿ ಆರಾಧನೆಯ ಪ್ರಮುಖ ಕೇಂದ್ರಗಳು ಧಿಕ್ರ್, ದೇವರ ಸ್ಮರಣೆಯ ಅಭ್ಯಾಸವನ್ನು ಒಳಗೊಂಡಿವೆ. ಸೂಫಿಗಳು ತಮ್ಮ ಮಿಷನರಿ ಮತ್ತು ಶೈಕ್ಷಣಿಕ ಚಟುವಟಿಕೆಗಳ ಮೂಲಕ ಇಸ್ಲಾಂ ಧರ್ಮವನ್ನು ಹರಡುವಲ್ಲಿ ಪ್ರಮುಖ ಪಾತ್ರ ವಹಿಸಿದ್ದಾರೆ.
                         </p>
                         <p>
-                        Despite a relative decline of Sufi orders in the modern era and attacks from fundamentalist Islamic movements (such as Salafism and Wahhabism), Sufism has continued to play an important role in the Islamic world. It has also influenced various forms of spirituality in the West and generated significant academic interest.
+                        ಆಧುನಿಕ ಯುಗದಲ್ಲಿ ಸೂಫಿ ಆದೇಶಗಳ ತುಲನಾತ್ಮಕ ಕುಸಿತ ಮತ್ತು ಮೂಲಭೂತವಾದಿ ಇಸ್ಲಾಮಿಕ್ ಚಳುವಳಿಗಳಿಂದ (ಸಲಾಫಿಸಂ ಮತ್ತು ವಹಾಬಿಸಂನಂತಹ) ದಾಳಿಗಳ ಹೊರತಾಗಿಯೂ, ಇಸ್ಲಾಮಿಕ್ ಜಗತ್ತಿನಲ್ಲಿ ಸೂಫಿಸಂ ಪ್ರಮುಖ ಪಾತ್ರವನ್ನು ವಹಿಸುವುದನ್ನು ಮುಂದುವರೆಸಿದೆ. ಇದು ಪಶ್ಚಿಮದಲ್ಲಿ ವಿವಿಧ ರೀತಿಯ ಆಧ್ಯಾತ್ಮಿಕತೆಯ ಮೇಲೆ ಪ್ರಭಾವ ಬೀರಿದೆ ಮತ್ತು ಗಮನಾರ್ಹವಾದ ಶೈಕ್ಷಣಿಕ ಆಸಕ್ತಿಯನ್ನು ಉಂಟುಮಾಡಿದೆ.
                         </p>
                     </div>
                 </div>
             </div>
         </div>
+
         <div className="bg-light py-5">
             <div className="container my-5">
                 <div className="row my-5">
                     <div className="col-12">
-                        <h1 className="text-center display-1">Mu'in al-Din Chishti</h1>
+                        <h1 className="text-center display-1">ಮುಯಿನ್ ಅಲ್-ದಿನ್ ಚಿಶ್ತಿ</h1>
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-6">
-                        <img src="https://www.ajmerdargahsharif.com/img/about-ajmer-dargah.jpg" className="img-fluid" alt="" />
+                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 my-5">
+                        <img src="https://www.ajmerdargahsharif.com/img/about-ajmer-dargah.jpg" className="img-fluid w-100 rounded img-thumbnail" alt="" />
                     </div>
-                    <div className="col-6">
+                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 my-5">
                         <div className="lead">
-                        <p className="lh-lg">Mu'in al-Din Hasan Chishti Sijzi (Persian: معین الدین چشتی, romanized: Muʿīn al-Dīn Chishtī; February 1143 – March 1236), known reverentially as Khawaja Gharib Nawaz (Persian: خواجہ غریب نواز, romanized: Khawāja Gharīb Nawāz), was a Persian Islamic scholar and mystic from Sistan, who eventually ended up settling in the Indian subcontinent in the early 13th-century, where he promulgated the Chishtiyya order of Sunni mysticism. This particular Tariqa (order) became the dominant Islamic spiritual order in medieval India. Most of the Indian Sunni saints are Chishti in their affiliation, including Nizamuddin Awliya (d. 1325) and Amir Khusrow (d. 1325)</p>
+                        <p className="lh-lg">ಮುಯಿನ್ ಅಲ್-ದಿನ್ ಹಸನ್ ಚಿಶ್ತಿ ಸಿಜ್ಜಿ (ಪರ್ಷಿಯನ್: معین الدین چشتی, ರೋಮನೈಸ್ಡ್: Muʿīn al-Dīn Chishtī; ಫೆಬ್ರುವರಿ 1143 - ಮಾರ್ಚ್ 1236), ಪೂಜ್ಯಪೂರ್ವಕವಾಗಿ ಖವಾಜಾ ಘರಿಬ್ ನವಾಜ್ (ಪರ್ಷಿಯನ್: جہی ನೋವಾಸ್, ರೋಮನೈಸ್ಡ್: ಖವಾಜಾ ಘರೀಬ್ ನವಾಜ್), ಪರ್ಷಿಯನ್ ಇಸ್ಲಾಮಿಕ್ ವಿದ್ವಾಂಸ ಮತ್ತು ಸಿಸ್ತಾನ್‌ನ ಅತೀಂದ್ರಿಯರಾಗಿದ್ದರು, ಅವರು ಅಂತಿಮವಾಗಿ 13 ನೇ ಶತಮಾನದ ಆರಂಭದಲ್ಲಿ ಭಾರತೀಯ ಉಪಖಂಡದಲ್ಲಿ ನೆಲೆಸಿದರು, ಅಲ್ಲಿ ಅವರು ಸುನ್ನಿ ಆಧ್ಯಾತ್ಮದ ಚಿಶ್ತಿಯಾ ಕ್ರಮವನ್ನು ಘೋಷಿಸಿದರು. ಈ ನಿರ್ದಿಷ್ಟ ತಾರಿಕಾ (ಆದೇಶ) ಮಧ್ಯಕಾಲೀನ ಭಾರತದಲ್ಲಿ ಪ್ರಬಲವಾದ ಇಸ್ಲಾಮಿಕ್ ಆಧ್ಯಾತ್ಮಿಕ ಕ್ರಮವಾಯಿತು. ನಿಜಾಮುದ್ದೀನ್ ಅವ್ಲಿಯಾ (ಡಿ. 1325) ಮತ್ತು ಅಮೀರ್ ಖುಸ್ರೋ (ಡಿ. 1325) ಸೇರಿದಂತೆ ಹೆಚ್ಚಿನ ಭಾರತೀಯ ಸುನ್ನಿ ಸಂತರು ತಮ್ಮ ಸಂಬಂಧದಲ್ಲಿ ಚಿಶ್ತಿಯಾಗಿದ್ದಾರೆ.</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
         
-        
         <div className="container my-5">
             <div className="row text-center">
-                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4">
-                        <div className="card">
-                            <img className="card-img-top" src="https://i.ytimg.com/vi/NXXbHLIr01Y/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AHUBoAC4AOKAgwIABABGEkgZShaMA8=&rs=AOn4CLDWhZTNwg_a2ikAhav15ui01xfI7A" alt="Title" />
-                            <div className="card-body">
-                                <h4 className="card-title">Hazrath Sufi Abdul Rehman Shah Baba</h4>
-                                <p className="card-text">Belapu</p>
-                            </div>
-                        </div>                    
-                    </div>
-                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4">
-                        <div className="card">
-                            <img className="card-img-top" src="https://i.ytimg.com/vi/NXXbHLIr01Y/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AHUBoAC4AOKAgwIABABGEkgZShaMA8=&rs=AOn4CLDWhZTNwg_a2ikAhav15ui01xfI7A" alt="Title" />
-                            <div className="card-body">
-                                <h4 className="card-title">Hazrath Sufi Abdul Rehman Shah Baba</h4>
-                                <p className="card-text">Belapu</p>
-                            </div>
+                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4">
+                    <div className="card">
+                        <img className="card-img-top" src="https://i.ytimg.com/vi/NXXbHLIr01Y/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AHUBoAC4AOKAgwIABABGEkgZShaMA8=&rs=AOn4CLDWhZTNwg_a2ikAhav15ui01xfI7A" alt="Title" />
+                        <div className="card-body">
+                            <h4 className="card-title">Hazrath Sufi Abdul Rehman Shah Baba</h4>
+                            <p className="card-text">Belapu</p>
+                        </div>
+                    </div>                    
+                </div>
+                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4">
+                    <div className="card">
+                        <img className="card-img-top" src="https://i.ytimg.com/vi/NXXbHLIr01Y/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AHUBoAC4AOKAgwIABABGEkgZShaMA8=&rs=AOn4CLDWhZTNwg_a2ikAhav15ui01xfI7A" alt="Title" />
+                        <div className="card-body">
+                            <h4 className="card-title">Hazrath Sufi Abdul Rehman Shah Baba</h4>
+                            <p className="card-text">Belapu</p>
                         </div>
                     </div>
-                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4">
-                        <div className="card">
-                            <img className="card-img-top" src="https://i.ytimg.com/vi/NXXbHLIr01Y/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AHUBoAC4AOKAgwIABABGEkgZShaMA8=&rs=AOn4CLDWhZTNwg_a2ikAhav15ui01xfI7A" alt="Title" />
-                            <div className="card-body">
-                                <h4 className="card-title">Hazrath Sufi Abdul Rehman Shah Baba</h4>
-                                <p className="card-text">Belapu</p>
-                            </div>
+                </div>
+                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4">
+                    <div className="card">
+                        <img className="card-img-top" src="https://i.ytimg.com/vi/NXXbHLIr01Y/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AHUBoAC4AOKAgwIABABGEkgZShaMA8=&rs=AOn4CLDWhZTNwg_a2ikAhav15ui01xfI7A" alt="Title" />
+                        <div className="card-body">
+                            <h4 className="card-title">Hazrath Sufi Abdul Rehman Shah Baba</h4>
+                            <p className="card-text">Belapu</p>
                         </div>
                     </div>
+                </div>
             </div>
         </div>
         
