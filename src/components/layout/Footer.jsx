6df591199c52efb81f0324976c2fbf4d5195709c
javedalihosamani <javed.hosamani007@gmail.com>
2025-01-02 21:05:58 +0530

    Banner Updated

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className="container">
             <div className="row">
                 <div className="col p-5">
-                  <p className="text-center lead p-3 fw-medium">&#169; ಹಜರತ್ ಸೂಫಿ ಅಬ್ದುಲ್ ರೆಹಮಾನ್ ಶಾ ಬಾಬಾ ಅವರಿಂದ ಎಲ್ಲಾ ಹಕ್ಕುಗಳನ್ನು ಕಾಯ್ದಿರಿಸಲಾಗಿದೆ</p>
+                  <p className="text-center lead p-3 text-dark fw-medium">&#169; ಹಜರತ್ ಸೂಫಿ ಅಬ್ದುಲ್ ರೆಹಮಾನ್ ಶಾ ಬಾಬಾ ಅವರಿಂದ ಎಲ್ಲಾ ಹಕ್ಕುಗಳನ್ನು ಕಾಯ್ದಿರಿಸಲಾಗಿದೆ</p>
                 </div>
             </div>
         </div>
