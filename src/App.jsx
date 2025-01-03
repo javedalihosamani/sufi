3ca56e2d5ff81827eb1b2acc52886d5b18f5f7c8
javedalihosamani <javed.hosamani007@gmail.com>
2025-01-04 00:21:12 +0530

    Music volume updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     const audio = new Audio(sounds);
     audio ? audio.play() : audio.pause();
     audio.loop = true;
-    audio.volume = 0.003;
+    audio.volume = 0.005;
   }
   useEffect(() => {playAudio()},[])
   return (
